17084e215e7e6fbb484ebbec50c909f1709a7172
admin <admin@devstage9.localhost>
2016-07-07 10:06:25 -0400

    order3 and order4 code

--- src/test/java/com/pizzahut/pizza/order/OrderTest.java
+++ src/test/java/com/pizzahut/pizza/order/OrderTest.java
@@ -55,105 +55,21 @@ public class OrderTest {
 		cart.addPizza(new Pizza(PizzaDB.Margherita_Medium.getPizza()));
 		double cost = pizzaHutOnlineShop.placeOrder();
 		Assert.assertEquals(562.5, cost);
-		
-		pizzaHutOnlineShop = new PizzaHutOnlineShop();
-		cart = new Cart();
-		order = new Order(cart);
-		pizzaHutOnlineShop.setOrder(order);
-		Pizza pizza1 = new Pizza(PizzaDB.ChickenMaxicana_Medium.getPizza());
-		pizza1.addAdditionalTopping(ToppingDB.Olives.getTopping());
-		cart.addPizza(pizza1); 
-		cart.addPizza(new Pizza(PizzaDB.Margherita_Medium.getPizza()));
-		cost = pizzaHutOnlineShop.placeOrder();
-		Assert.assertEquals(581.25, cost);
-		
-		pizzaHutOnlineShop = new PizzaHutOnlineShop();
-		cart = new Cart();
-		order = new Order(cart);
-		pizzaHutOnlineShop.setOrder(order);
-		pizza1 = new Pizza(PizzaDB.ChickenMaxicana_Medium.getPizza());
-		pizza1.setCrust(CrustDB.CheeseBurst.getCrust());;
-		cart.addPizza(pizza1); 
-		cart.addPizza(new Pizza(PizzaDB.Margherita_Medium.getPizza()));
-		cost = pizzaHutOnlineShop.placeOrder();
-		Assert.assertEquals(812.5, cost);
-		
-		pizzaHutOnlineShop = new PizzaHutOnlineShop();
-		cart = new Cart();
-		order = new Order(cart);
-		pizzaHutOnlineShop.setOrder(order);
-		pizza1 = new Pizza(PizzaDB.VegHawaiianDelight_Medium.getPizza());
-		pizza1.addAdditionalTopping(ToppingDB.Olives.getTopping());
-		pizza1.addAdditionalTopping(ToppingDB.Cheese.getTopping());
-		pizza1.setCrust(CrustDB.PanPizza.getCrust());;
-		cart.addPizza(pizza1); 
-		Pizza pizza2 = new Pizza(PizzaDB.NonVegHawaiianDelight_Medium.getPizza());
-		pizza2.addAdditionalTopping(ToppingDB.BarbequeChicken.getTopping());
-		pizza2.setCrust(CrustDB.CheeseBurst.getCrust());;
-		cart.addPizza(pizza2); 
-		cost = pizzaHutOnlineShop.placeOrder();
-		Assert.assertEquals(1068.75, cost);
-		
-		pizzaHutOnlineShop = new PizzaHutOnlineShop();
-		cart = new Cart();
-		order = new Order(cart);
-		pizzaHutOnlineShop.setOrder(order);
-		cart.addPizza(new Pizza(PizzaDB.VeggieParadise_Medium.getPizza())); 
-		cart.addPizza(new Pizza(PizzaDB.ZestyChicken_Medium.getPizza()));
-		cart.addPizza(new Pizza(PizzaDB.ChickenMaxicana_Medium.getPizza()));
-		cost = pizzaHutOnlineShop.placeOrder();
-		Assert.assertEquals(1012.5, cost);
-		
-		
-		pizzaHutOnlineShop = new PizzaHutOnlineShop();
-		cart = new Cart();
-		order = new Order(cart);
-		pizzaHutOnlineShop.setOrder(order);
-		pizza1 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
-		pizza2 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
-		Pizza pizza3 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
-		Pizza pizza4 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
-		pizza1.removeDefaultTopping(ToppingDB.Capsicum.getTopping());
-		pizza2.removeDefaultTopping(ToppingDB.Capsicum.getTopping());
-		
-		pizza1.addAdditionalTopping(ToppingDB.Olives.getTopping());
-		pizza2.addAdditionalTopping(ToppingDB.Olives.getTopping());
-		
-		pizza1.addAdditionalTopping(ToppingDB.BabyCorn.getTopping());
-		pizza2.addAdditionalTopping(ToppingDB.BabyCorn.getTopping());
-		
-		pizza3.addAdditionalTopping(ToppingDB.Cheese.getTopping());
-		pizza4.addAdditionalTopping(ToppingDB.Cheese.getTopping());
-		
-		pizza1.setCrust(CrustDB.ThinCrust.getCrust());;
-		pizza2.setCrust(CrustDB.ThinCrust.getCrust());;
-		pizza3.setCrust(CrustDB.ThinCrust.getCrust());;
-		pizza4.setCrust(CrustDB.ThinCrust.getCrust());;
-		
-		Pizza pizza5 = new Pizza(PizzaDB.Margherita_Medium.getPizza());
-		Pizza pizza6 = new Pizza(PizzaDB.VegHawaiianDelight_Medium.getPizza());
-		
-		Pizza pizza7 = new Pizza(PizzaDB.ZestyChicken_Medium.getPizza());
-		
-		pizza7.setCrust(CrustDB.CheeseBurst.getCrust());;
-		
-		pizza7.removeDefaultTopping(ToppingDB.Paprika.getTopping());
-		
-		pizza7.addAdditionalTopping(ToppingDB.BabyCorn.getTopping());
-		pizza7.addAdditionalTopping(ToppingDB.Olives.getTopping());
-		pizza7.addAdditionalTopping(ToppingDB.Onion.getTopping());
-		
-		cart.addPizza(pizza1);
-		cart.addPizza(pizza2);
-		cart.addPizza(pizza3);
-		cart.addPizza(pizza4);
-		cart.addPizza(pizza5);
-		cart.addPizza(pizza6);
-		cart.addPizza(pizza7);
-		cost = pizzaHutOnlineShop.placeOrder();
-		Assert.assertEquals(3093.75, cost);
-		
-		
+	}
+
+	@Test
+	public void shouldGiveTotalCostAfterOrderPlcaed7() {
+		PizzaHutOnlineShop pizzaHutOnlineShop;
+		Cart cart;
+		Order order;
+		double cost;
+		Pizza pizza1;
+		Pizza pizza2;
+		Pizza pizza3;
+		Pizza pizza4;
+		Pizza pizza5;
+		Pizza pizza6;
+		Pizza pizza7;
 		pizzaHutOnlineShop = new PizzaHutOnlineShop();
 		cart = new Cart();
 		order = new Order(cart);
@@ -204,6 +120,141 @@ public class OrderTest {
 		cart.addPizza(pizza7);
 		cost = pizzaHutOnlineShop.placeOrder();
 		Assert.assertEquals(3881.25, cost);
+	}
+
+	@Test
+	public void shouldGiveTotalCostAfterOrderPlcaed6() {
+		PizzaHutOnlineShop pizzaHutOnlineShop;
+		Cart cart;
+		Order order;
+		double cost;
+		Pizza pizza1;
+		Pizza pizza2;
+		pizzaHutOnlineShop = new PizzaHutOnlineShop();
+		cart = new Cart();
+		order = new Order(cart);
+		pizzaHutOnlineShop.setOrder(order);
+		pizza1 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
+		pizza2 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
+		Pizza pizza3 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
+		Pizza pizza4 = new Pizza(PizzaDB.PeppyPaneer_Medium.getPizza());
+		pizza1.removeDefaultTopping(ToppingDB.Capsicum.getTopping());
+		pizza2.removeDefaultTopping(ToppingDB.Capsicum.getTopping());
+		
+		pizza1.addAdditionalTopping(ToppingDB.Olives.getTopping());
+		pizza2.addAdditionalTopping(ToppingDB.Olives.getTopping());
+		
+		pizza1.addAdditionalTopping(ToppingDB.BabyCorn.getTopping());
+		pizza2.addAdditionalTopping(ToppingDB.BabyCorn.getTopping());
+		
+		pizza3.addAdditionalTopping(ToppingDB.Cheese.getTopping());
+		pizza4.addAdditionalTopping(ToppingDB.Cheese.getTopping());
+		
+		pizza1.setCrust(CrustDB.ThinCrust.getCrust());;
+		pizza2.setCrust(CrustDB.ThinCrust.getCrust());;
+		pizza3.setCrust(CrustDB.ThinCrust.getCrust());;
+		pizza4.setCrust(CrustDB.ThinCrust.getCrust());;
+		
+		Pizza pizza5 = new Pizza(PizzaDB.Margherita_Medium.getPizza());
+		Pizza pizza6 = new Pizza(PizzaDB.VegHawaiianDelight_Medium.getPizza());
+		
+		Pizza pizza7 = new Pizza(PizzaDB.ZestyChicken_Medium.getPizza());
+		
+		pizza7.setCrust(CrustDB.CheeseBurst.getCrust());;
+		
+		pizza7.removeDefaultTopping(ToppingDB.Paprika.getTopping());
+		
+		pizza7.addAdditionalTopping(ToppingDB.BabyCorn.getTopping());
+		pizza7.addAdditionalTopping(ToppingDB.Olives.getTopping());
+		pizza7.addAdditionalTopping(ToppingDB.Onion.getTopping());
+		
+		cart.addPizza(pizza1);
+		cart.addPizza(pizza2);
+		cart.addPizza(pizza3);
+		cart.addPizza(pizza4);
+		cart.addPizza(pizza5);
+		cart.addPizza(pizza6);
+		cart.addPizza(pizza7);
+		cost = pizzaHutOnlineShop.placeOrder();
+		Assert.assertEquals(3093.75, cost);
+	}
+
+	@Test
+	public void shouldGiveTotalCostAfterOrderPlcaed5() {
+		PizzaHutOnlineShop pizzaHutOnlineShop;
+		Cart cart;
+		Order order;
+		double cost;
+		pizzaHutOnlineShop = new PizzaHutOnlineShop();
+		cart = new Cart();
+		order = new Order(cart);
+		pizzaHutOnlineShop.setOrder(order);
+		cart.addPizza(new Pizza(PizzaDB.VeggieParadise_Medium.getPizza())); 
+		cart.addPizza(new Pizza(PizzaDB.ZestyChicken_Medium.getPizza()));
+		cart.addPizza(new Pizza(PizzaDB.ChickenMaxicana_Medium.getPizza()));
+		cost = pizzaHutOnlineShop.placeOrder();
+		Assert.assertEquals(1012.5, cost);
+	}
+	
+	@Test
+	public void shouldGiveTotalCostAfterOrderPlcaed4() {
+		PizzaHutOnlineShop pizzaHutOnlineShop;
+		Cart cart;
+		Order order;
+		double cost;
+		Pizza pizza1;
+		pizzaHutOnlineShop = new PizzaHutOnlineShop();
+		cart = new Cart();
+		order = new Order(cart);
+		pizzaHutOnlineShop.setOrder(order);
+		pizza1 = new Pizza(PizzaDB.VegHawaiianDelight_Medium.getPizza());
+		pizza1.addAdditionalTopping(ToppingDB.Olives.getTopping());
+		pizza1.addAdditionalTopping(ToppingDB.Cheese.getTopping());
+		pizza1.setCrust(CrustDB.PanPizza.getCrust());;
+		cart.addPizza(pizza1); 
+		Pizza pizza2 = new Pizza(PizzaDB.NonVegHawaiianDelight_Medium.getPizza());
+		pizza2.addAdditionalTopping(ToppingDB.BarbequeChicken.getTopping());
+		pizza2.setCrust(CrustDB.CheeseBurst.getCrust());;
+		cart.addPizza(pizza2); 
+		cost = pizzaHutOnlineShop.placeOrder();
+		Assert.assertEquals(1068.75, cost);
+	}
+
+	@Test
+	public void shouldGiveTotalCostAfterOrderPlcaed3() {
+		PizzaHutOnlineShop pizzaHutOnlineShop;
+		Cart cart;
+		Order order;
+		double cost;
+		Pizza pizza1;
+		pizzaHutOnlineShop = new PizzaHutOnlineShop();
+		cart = new Cart();
+		order = new Order(cart);
+		pizzaHutOnlineShop.setOrder(order);
+		pizza1 = new Pizza(PizzaDB.ChickenMaxicana_Medium.getPizza());
+		pizza1.setCrust(CrustDB.CheeseBurst.getCrust());;
+		cart.addPizza(pizza1); 
+		cart.addPizza(new Pizza(PizzaDB.Margherita_Medium.getPizza()));
+		cost = pizzaHutOnlineShop.placeOrder();
+		Assert.assertEquals(812.5, cost);
+	}
+
+	@Test
+	public void shouldGiveTotalCostAfterOrderPlcaed2() {
+		PizzaHutOnlineShop pizzaHutOnlineShop;
+		Cart cart;
+		Order order;
+		double cost;
+		pizzaHutOnlineShop = new PizzaHutOnlineShop();
+		cart = new Cart();
+		order = new Order(cart);
+		pizzaHutOnlineShop.setOrder(order);
+		Pizza pizza1 = new Pizza(PizzaDB.ChickenMaxicana_Medium.getPizza());
+		pizza1.addAdditionalTopping(ToppingDB.Olives.getTopping());
+		cart.addPizza(pizza1); 
+		cart.addPizza(new Pizza(PizzaDB.Margherita_Medium.getPizza()));
+		cost = pizzaHutOnlineShop.placeOrder();
+		Assert.assertEquals(581.25, cost);
 	}
 	
 	@Test
